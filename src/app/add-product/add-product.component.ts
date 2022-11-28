887c0ebb3eb8373deb1d07bfba0cf68a6822f9ce
fasilmsageer <fasilmsageer@gmail.com>
2022-11-28 16:12:24 +0530

    addvalue

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConnectableObservable } from 'rxjs';
 
 @Component({
   selector: 'app-add-product',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
+  
+  title=""
+  price=""
+  image=""
+  description=""
+  category=""
+  rating=""
+  
+
+  readValues=()=>
+  {
+
+    let data:any={"title":this.title,"price":this.price,"description":this.description,"category":this.category,"image":this.image,"rating":this.rating}
+    console.log(data)
+  }
 
 }
